97741ae16d3782448e8c5b4c7a6be6f16069f6ae
Ochirkhuyag.L <ochkoo@gmail.com>
2009-05-11 13:31:43 +0900

    adding resizable to wmd editor

--- lib/wmd_on_rails.rb
+++ lib/wmd_on_rails.rb
@@ -40,5 +40,52 @@ EOF
 
       javascript_tag js
     end
+    
+    def wmd_textarea(object, field, options = {})
+      var = instance_variable_get("@#{object}")
+      if var
+        value = var.send(field.to_sym)
+        value = value.nil? ? "" : value
+      else
+        value = ""
+        klass = "#{object}".camelcase.constantize
+        instance_variable_set("@#{object}", eval("#{klass}.new()"))
+      end
+      id = wmd_element_id(object, field)
+
+      cols = options[:cols].nil? ? '' : "cols='"+options[:cols]+"'"
+      rows = options[:rows].nil? ? '' : "rows='"+options[:rows]+"'"
+
+      width = options[:width].nil? ? '100%' : options[:width]
+      height = options[:height].nil? ? '100%' : options[:height]
+
+      html_class = options[:class].nil? ? 'wmd-textarea' : options[:class]
+      if options[:ajax]
+        inputs = "<input type='hidden' id='#{id}_hidden' name='#{object}[#{field}]'>\n" <<
+                 "<textarea id='#{id}' #{cols} #{rows} name='#{id}' class='#{html_class}'>#{value}</textarea>\n"
+      else
+        inputs = "<textarea id='#{id}' #{cols} #{rows} name='#{object}[#{field}]' class='#{html_class}'>#{value}</textarea>\n"
+      end
+
+      return inputs <<
+        create_wmd(id)
+    end
+    
+    def wmd_resizable_textarea(object, field, options ={})
+      content = wmd_textarea(object, field, :class=>'resizable')
+      return content <<
+        javascript_tag("$(document).ready(function() {$('textarea.resizable:not(.processed)').TextAreaResizer();});")
+    end
+    
+    def wmd_editor(object, field, options ={})
+      content = wmd_resizable_textarea(object, field, options)
+      return content << wmd_preview(:id => wmd_element_id(object, field))
+    end
+    
+    def wmd_element_id(object, field)
+      id = eval("@#{object}.id")
+      "#{object}_#{id}_#{field}_wmd"
+    end
+    
   end
 end
